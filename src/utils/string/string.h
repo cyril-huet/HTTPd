dfd6d3b22e7769294732889353ea7635e6b603e3
Cyril Huet <cyril.huet@epita.fr>
2026-09-24 13:46:15 +0200

    refactor: simplify string utilities

--- src/utils/string/string.h
+++ src/utils/string/string.h
@@ -10,36 +10,13 @@ struct string
     char *data;
 };
 
-/*
- ** @brief Create new string struct from char * and size
- **        Be careful, the argument str will not be deallocated and thus you
- **        can call this function with a static char *
- **
- ** @param str
- ** @param size
- **
- ** @return the newly allocated string
- */
+/* Create a new string and copy size characters from data. */
 struct string *string_create(const char *str, size_t size);
 
-/*
- ** @brief Act like memcmp(3) but for struct string and char *
- **
- ** @param str1
- ** @param str2
- ** @param n
- **
- ** @return
- */
+/* Compare the string with the first size characters of data. */
 int string_compare_n_str(const struct string *str1, const char *str2, size_t n);
 
-/*
- ** @brief Concat a char * with its size in a struct string
- **
- ** @param str
- ** @param to_concat
- ** @param size
- */
+/* Append size characters to the string. */
 void string_concat_str(struct string *str, const char *to_concat, size_t size);
 
 void string_destroy(struct string *str);
