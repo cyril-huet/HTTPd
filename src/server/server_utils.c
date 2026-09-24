4fc7cc2ad33c9372a345f62dd617f152c3904e9c
Cyril Huet <cyril.huet@epita.fr>
2026-09-24 11:04:57 +0200

    fix: improve HTTP request reading

--- src/server/server_utils.c
+++ src/server/server_utils.c
@@ -1,33 +1,54 @@
-
 #define _POSIX_C_SOURCE 200809L
 #include "server_utils.h"
 
+#include <errno.h>
 #include <stddef.h>
 #include <unistd.h>
 
-int read_request(int client_fd, char *buffer, size_t size)
+static int headers_are_complete(const char *buffer, size_t size)
 {
-    size_t index = 0;
-    while (index < size)
+    if (size < 4)
     {
-        int temps = read(client_fd, buffer + index, size - index);
-        if (temps <= 0)
+        return 0;
+    }
+
+    for (size_t index = 3; index < size; index++)
+    {
+        if (buffer[index - 3] == '\r' && buffer[index - 2] == '\n'
+            && buffer[index - 1] == '\r' && buffer[index] == '\n')
         {
-            return -1;
+            return 1;
         }
-        for (int i = 0; i < temps; i++)
-        {
-            index++;
-        }
-        if (index < 4)
+    }
+
+    return 0;
+}
+
+int read_request(int client_fd, char *buffer, size_t capacity)
+{
+    size_t total = 0;
+
+    while (total < capacity)
+    {
+        ssize_t bytes_read = read(client_fd, buffer + total, capacity - total);
+
+        if (bytes_read < 0 && errno == EINTR)
         {
             continue;
         }
-        if (buffer[index - 1] == '\n' && buffer[index - 2] == '\r'
-            && buffer[index - 3] == '\n' && buffer[index - 4] == '\r')
+
+        if (bytes_read <= 0)
         {
-            return index;
+            return -1;
+        }
+
+        total += bytes_read;
+
+        if (headers_are_complete(buffer, total) == 1)
+        {
+            return total;
         }
     }
-    return index;
+
+    return -1;
 }
